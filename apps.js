19b398ec7006e332eaeb92c88a5f48221b4c9a60
ritesh752 <yadavritesh00000@gmail.com>
2026-01-03 16:04:28 +0530

    css change

--- apps.js
+++ apps.js
@@ -1,14 +1,19 @@
 const counterDisplay = document.getElementById("counter");
 const decButtons = document.getElementsByClassName("decrease");
 const incButtons = document.getElementsByClassName("increase");
+const clButtons = document.getElementsByClassName("clear");
 
 let counter = 0;
-decButtons[0].addEventListener("click",function(){
-    counter--;
-    counterDisplay.innerText = counter;
-})
+decButtons[0].addEventListener("click", function () {
+  counter--;
+  counterDisplay.innerText = counter;
+});
 
-incButtons[0].addEventListener("click",function(){
-    counter++;
-    counterDisplay.innerText = counter;
-})
+incButtons[0].addEventListener("click", function () {
+  counter++;
+  counterDisplay.innerText = counter;
+});
+clButtons[0].addEventListener("click", function () {
+  counter = 0;
+  counterDisplay.innerText = counter;
+});
